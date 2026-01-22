89277b0506b96b2d089f7af4574bc997540d1548
Гавриил Михайлов <gmikay@proton.me>
2026-01-22 10:35:50 +0300

    Initial Commit

---
+++ QuizMates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizMates
+//
+//  Created by Gavriil Mikhailov on 22.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
